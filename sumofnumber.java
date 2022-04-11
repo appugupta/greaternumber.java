2f53cbf5e048d22058422803810f1ced34f789a6
appugupta <91661911+appugupta@users.noreply.github.com>
2022-04-11 11:55:29 +0530

    Add files via upload

---
+++ sumofnumber.java
@@ -0,0 +1,21 @@
+package kiet;
+
+public class sum {
+
+	public static void main(String[] args) {
+		
+				// TODO Auto-generated method stub
+				  
+				int n1 = 225, n2 = 115, sum;  
+				sum = n1 + n2;  
+				System.out.println("The sum of numbers is: "+sum);  
+				}  
+				
+
+			
+
+
+
+
+	}
+
